3f3161e33aede4a226d69b5e817137aed901e693
bb0143sbw <bb0143sbw+github@gmail.com>
2023-12-11 16:14:01 +0300

    map location updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
           "rgb(227, 0, 14, 0.4) 5px 5px, rgb(227, 0, 14, 0.3) 10px 10px, rgb(227, 0, 14, 0.2) 15px 15px, rgb(227, 0, 14, 0.1) 20px 20px, rgb(227, 0, 14, 0.05) 25px 25px",
         bottom:
           "rgb(227, 0, 14, 0.4) 0px 5px, rgb(227, 0, 14, 0.3) 0px 10px, rgb(227, 0, 14, 0.2) 0px 15px, rgb(227, 0, 14, 0.1) 0px 20px, rgb(227, 0, 14, 0.05) 0px 25px",
-        painting: "rgb(38, 57, 77) 0px 20px 30px -10px;",
+        painting: "rgb(38, 57, 77) 0px 20px 30px -10px",
       },
     },
 
